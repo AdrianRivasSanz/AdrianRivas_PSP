d72b150acc6ef212a8c27b37f3811b7fb32cc093
Adrian <adrianrivassanz@gmail.com>
2022-12-15 14:25:40 +0100

    update

---
+++ Entrega_Red/Cliente/src/Cliente3.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+
+public class Cliente3 {
+    public static void main(String[] args) {
+        try {
+            Socket cliente = new Socket("127.0.0.1",9999);
+
+            ObjectInputStream inObjeto = new ObjectInputStream(cliente.getInputStream());
+
+            Persona persona = (Persona)inObjeto.readObject();
+            System.out.println(persona.getNombre());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
